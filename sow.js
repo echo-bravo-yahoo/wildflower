e51673a50568d5178d2d76966779e14451e90eee
Ashton Eby <ashton.eby@gmail.com>
2019-12-23 20:11:38 -0800

    Fix sowing; now uses recursive-copy

--- sow.js
+++ sow.js
@@ -1,12 +1,16 @@
 const fse = require('fs-extra')
+const copy = require('recursive-copy')
 const path = require('path')
 const meadows = require('./meadows')
-const { fixSrcPath, fixDestPath } = require('./common')
+// The functions are named relative to gathering, not sowing
+// So we'll have to use them named in reverse
+const { fixSrcPath: fixDestPath, fixDestPath: fixSrcPath } = require('./common')
 
 promises = []
 
 meadows.forEach((meadow) => {
-    promises.push(fse.copyFile(fixDestPath(meadow.path), fixSrcPath(meadow.path))) 
+    promises.push(copy(fixSrcPath(meadow.path), fixDestPath(meadow.path))
+        .catch(console.warn))
 })    
 
 Promise.all(promises).then(() => console.log('Done sowing.')).catch((err) => console.error('Error while sowing:', err))
